2897126014a20b79f13561f615927d2b5808de33
nguetcheu <dnguetcheu@gmail.com>
2024-01-17 20:47:11 +0100

    Suppression client par admin

--- src/app/components/admin-client/admin-client.component.ts
+++ src/app/components/admin-client/admin-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AdminService } from 'src/app/service/admin.service';
 
 @Component({
@@ -9,7 +10,10 @@ import { AdminService } from 'src/app/service/admin.service';
 export class AdminClientComponent implements OnInit {
   utilisateurs: any[] = [];
 
-  constructor(private adminService: AdminService) {}
+  constructor(
+    private adminService: AdminService,
+    private firestore: AngularFirestore
+  ) {}
 
   ngOnInit(): void {
     // @ts-ignore
@@ -18,5 +22,13 @@ export class AdminClientComponent implements OnInit {
       .subscribe((utilisateurs) => {
         this.utilisateurs = utilisateurs;
       });
+  }
+
+  // Suppresion d'un commande de la collection
+  deleteUtilisateur(utilisateurId: string): Promise<void> {
+    const utilisateurRef = this.firestore
+      .collection('utilisateurs')
+      .doc(utilisateurId);
+    return utilisateurRef.delete();
   }
 }
