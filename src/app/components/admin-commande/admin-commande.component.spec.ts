d5ce84229341defdef76c550f4eb99368f3f0aa0
nguetcheu <dnguetcheu@gmail.com>
2024-03-28 14:51:52 +0100

    Ajout de test sytéme

--- src/app/components/admin-commande/admin-commande.component.spec.ts
+++ src/app/components/admin-commande/admin-commande.component.spec.ts
@@ -1,25 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { AdminCommandeComponent } from './admin-commande.component';
+import { AdminService } from 'src/app/service/admin.service';
 
 describe('AdminCommandeComponent', () => {
   let component: AdminCommandeComponent;
-  let fixture: ComponentFixture<AdminCommandeComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AdminCommandeComponent ]
-    })
-    .compileComponents();
-  });
+  let adminService: AdminService;
+  let router: Router;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AdminCommandeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    adminService = jasmine.createSpyObj('AdminService', [
+      'getCommandes',
+      'deleteCommande',
+    ]); // Créer un mock pour AdminService
+    component = new AdminCommandeComponent(adminService, router); // Passer le mock de AdminService
   });
 
-  it('should create', () => {
+  it('il devra créer', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Il devra imprimer le tableau', () => {
+    const windowSpy = spyOn(window, 'print'); // Espionner la fonction window.print()
+
+    component.printTable();
+
+    expect(windowSpy).toHaveBeenCalled(); // Vérifier si window.print() a été appelé
   });
 });
