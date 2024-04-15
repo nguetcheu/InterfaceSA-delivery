9cc61b48f60242f3271c2f4024643a8a84c0035a
nguetcheu <dnguetcheu@gmail.com>
2024-04-15 22:57:52 +0200

    Ajouut variable environnement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDqHtDtjwLqPp7BQeoH4xyJww33guFnPVA',
+    authDomain: 'interface-delivery.firebaseapp.com',
+    projectId: 'interface-delivery',
+    storageBucket: 'interface-delivery.appspot.com',
+    messagingSenderId: '358001045063',
+    appId: '1:358001045063:web:04aeabe282ad934ac54b62',
+  },
 };
